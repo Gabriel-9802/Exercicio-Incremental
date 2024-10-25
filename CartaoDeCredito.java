e3725863e28860e541667d8345afa224a9658467
Gabriel-9802 <gsilvass98@gmail.com>
2024-10-25 15:16:33 -0300

    Update CartaoDeCredito.java
    git commit -m "Incremento 3: Sobrecarga de métodos para compra básica e compra com cashback"

--- CartaoDeCredito.java
+++ CartaoDeCredito.java
@@ -4,6 +4,15 @@ public class CartaoDeCredito {
     private String cpfTitular;
     private float limite;
     private float saldo;
+    private float taxaDeCashback;
+
+    public CartaoDeCredito(String numero, String nomeTitular, float limite) {
+        this.numero = numero;
+        this.nomeTitular = nomeTitular;
+        this.limite = limite;
+        this.saldo = limite;
+        this.taxaDeCashback = 0.0f; 
+    }
 
     public CartaoDeCredito(String numero, String nomeTitular, String cpfTitular, float limite) {
         this.numero = numero;
@@ -11,6 +20,34 @@ public class CartaoDeCredito {
         this.cpfTitular = cpfTitular;
         this.limite = limite;
         this.saldo = limite;
+        this.taxaDeCashback = 0.0f; 
+    }
+
+    public CartaoDeCredito(String numero, String nomeTitular, String cpfTitular, float limite, float taxaDeCashback) {
+        this.numero = numero;
+        this.nomeTitular = nomeTitular;
+        this.cpfTitular = cpfTitular;
+        this.limite = limite;
+        this.saldo = limite;
+        this.taxaDeCashback = taxaDeCashback;
+    }
+
+    public boolean realizarTransacao(float valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            return true;
+        } else {
+            System.out.println("Saldo insuficiente.");
+            return false;
+        }
+    }
+
+    public boolean realizarTransacao(float valor, boolean comCashback) {
+        if (comCashback && taxaDeCashback > 0) {
+            float valorComCashback = valor * (1 - taxaDeCashback);
+            return realizarTransacao(valorComCashback);
+        }
+        return realizarTransacao(valor);
     }
 
     public String getNumero() {
@@ -53,22 +90,19 @@ public class CartaoDeCredito {
         this.saldo = saldo;
     }
 
-    // Métodos usando Getters e Setters
+    public float getTaxaDeCashback() {
+        return taxaDeCashback;
+    }
+
+    public void setTaxaDeCashback(float taxaDeCashback) {
+        this.taxaDeCashback = taxaDeCashback;
+    }
+
     public float consultarSaldo() {
-        return getSaldo();
+        return saldo;
     }
 
     public float consultarLimite() {
-        return getLimite();
-    }
-
-    public boolean realizarTransacao(float valor) {
-        if (valor <= getSaldo()) {
-            setSaldo(getSaldo() - valor);
-            return true;
-        } else {
-            System.out.println("Saldo insuficiente.");
-            return false;
-        }
+        return limite;
     }
 }
